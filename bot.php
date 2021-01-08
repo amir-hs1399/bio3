847ee7ea0d1f9bf3d15e73463ef04713b4eed766
amir hs 1399 <75607507+amir-hs1399@users.noreply.github.com>
2021-01-08 16:52:48 +0330

    sss
    sss

---
+++ bot.php
@@ -0,0 +1,17 @@
+<?php
+ //یک دقیقه کرون جاب کنید
+   $host = "https://amirwolf512.ir/time/"; //محل سورس ساعت
+   unlink("session.madeline.lock");
+   unlink("MadelineProto.log");
+   unlink("madeline.phar.version");
+   unlink("madeline.phar");
+   unlink("madeline.php");
+   json_decode(file_get_contents("$host/amir1.php"),true);
+/*
+کانال مارو حمایت کنید 
+ @MadeLineproto_F 
+ 
+ اوپن شده توسط امیر ولف
+ @amir_wolf512
+*/
+ ?>
